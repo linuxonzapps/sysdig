b7349b28ab830fc51727adcd7978a582f9716aa3
rposts <rishi.investigate@gmail.com>
2026-03-23 14:53:03 +0000

    Add DISTRO processing
    Signed-off-by: rposts <rishi.investigate@gmail.com>

--- src/build.sh
+++ src/build.sh
@@ -3,6 +3,16 @@ set -e -o pipefail
 read -ra arr <<< "$@"
 version=${arr[1]}
 trap 0 1 2 ERR
+# Extract DISTRO details for tagging
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    DISTRO="$ID-$VERSION_ID"
+    if [ "$VERSION_CODENAME" != "" ]; then
+        DISTRO="$ID-$VERSION_CODENAME"
+    fi
+fi
+current_dir="$PWD"
+echo $DISTRO > .distro_zab.txt
 # Ensure sudo is installed
 apt-get update && apt-get install sudo rpm -y
 current_dir="$PWD"
